b616c0a333cbe8871043cf8cc57a3186934eae04
j0ezy <145462961+j0ezy@users.noreply.github.com>
2024-07-25 16:55:44 +0200

    Create one round of play
    Created a function  with two parameters
    (random and player choice) and decides who wins or
    if it's a tie

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ function getComputerChoice(){
     return randomChoiceName
 }
 
+// function to get the human choice value
 function getHumanChoice(){
     let humanChoice = prompt("Enter your choice(Rock, Paper or Scissors)").toLowerCase();
     if(!(humanChoice === "rock" || humanChoice === "scissors" || humanChoice === "paper")){
@@ -24,7 +25,61 @@ function getHumanChoice(){
     return humanChoice;
 }
 
+function playRound(randomChoice, humanChoice){
+    if(randomChoice === "rock"){
+        switch(humanChoice){
+            case "rock":
+                console.log("It's a tie! You both choose rock")
+                break;
+            case "paper":
+                console.log("You win! Paper beats rock")
+                humanScore++;
+                break;
+            case "scissors":
+                console.log("You lose! Scissors loses to rock")
+                computerScore++;
+                break;
+        }
+    }else if (randomChoice === "paper"){
+        switch(humanChoice){
+            case "rock":
+                console.log("You lose! Rock loses to paper")
+                computerScore++;
+                break;
+            case "paper":
+                console.log("It's a tie! You both choose paper")
+                break;
+            case "scissors":
+                console.log("You win! Scissors beat paper")
+                humanScore++;
+                break;
+        }
+    }else if (randomChoice === "scissors"){
+        switch(humanChoice){
+            case "rock":
+                console.log("You win! Rock beat scissors")
+                humanScore++;
+                break;
+            case "paper":
+                console.log("You lose! Scissors lose to paper")
+                computerScore++;
+                break;
+            case "scissors":
+                console.log("It's a tie! You both choose scissors")
+                break;
+        }
+    }else{
+        console.log("Error, something went wrong")
+    }
+}
+
 let randomChoice = getComputerChoice().toLowerCase();
 let humanChoice = getHumanChoice().toLowerCase();
-console.log(randomChoice)
-console.log(humanChoice)
+console.log("Computer choice: " + randomChoice)
+console.log("Player choice: " + humanChoice)
+let computerScore = 0;
+let humanScore = 0;
+console.log("Computer score: " + computerScore)
+console.log("Player score: " + humanScore)
+playRound(randomChoice,humanChoice);
+
