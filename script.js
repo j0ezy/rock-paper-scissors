09949cee0a59215a549634417f19748151aaaa09
j0ezy <145462961+j0ezy@users.noreply.github.com>
2024-07-25 16:28:46 +0200

    Create random choice of the computer
    Created two functions. First one generates a
    random value between two given integers and the
    second one converts the returned value to either
    Rock, Paper or Scissors

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello World")
+// function generates random number between two given numbers
+function randomBetweenTwoNumbers(min, max){
+    return Math.floor(Math.random() * (max - min) + min);
+}
+// function to get the string equivalent of the numerical representation
+function getComputerChoice(){
+    let randomChoice = randomBetweenTwoNumbers(1, 4)
+    let randomChoiceName = null;
+    if(randomChoice  === 1){
+        randomChoiceName = "Rock"
+    } else if(randomChoice  === 2) {
+        randomChoiceName = "Paper"
+    } else{
+        randomChoiceName = "Scissors"
+    }
+    return randomChoiceName
+}
+
+let randomChoice = getComputerChoice();
+console.log(randomChoice)
