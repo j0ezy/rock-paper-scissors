51e9bda03677e58cddf8733bfd1b25a3c1e1656e
j0ezy <145462961+j0ezy@users.noreply.github.com>
2024-07-25 17:35:22 +0200

    Create playGame function
    First I had to move around the functions a bit and
    change the parameters of some functions and then
    I created the playGame function which calls the
    playRound function 5 times and returns true/false/
    null values (used for scoring)

--- script.js
+++ script.js
@@ -25,7 +25,13 @@ function getHumanChoice(){
     return humanChoice;
 }
 
-function playRound(randomChoice, humanChoice){
+// play one round of Rock Paper Scissors
+function playRound(){
+
+    let randomChoice = getComputerChoice().toLowerCase();
+    let humanChoice = getHumanChoice().toLowerCase();
+    let score = null;
+
     if(randomChoice === "rock"){
         switch(humanChoice){
             case "rock":
@@ -33,36 +39,36 @@ function playRound(randomChoice, humanChoice){
                 break;
             case "paper":
                 console.log("You win! Paper beats rock")
-                humanScore++;
+                score = true;
                 break;
             case "scissors":
                 console.log("You lose! Scissors loses to rock")
-                computerScore++;
+                score = false;
                 break;
         }
     }else if (randomChoice === "paper"){
         switch(humanChoice){
             case "rock":
                 console.log("You lose! Rock loses to paper")
-                computerScore++;
+                score = false;
                 break;
             case "paper":
                 console.log("It's a tie! You both choose paper")
                 break;
             case "scissors":
                 console.log("You win! Scissors beat paper")
-                humanScore++;
+                score = true;
                 break;
         }
     }else if (randomChoice === "scissors"){
         switch(humanChoice){
             case "rock":
                 console.log("You win! Rock beat scissors")
-                humanScore++;
+                score = true;
                 break;
             case "paper":
                 console.log("You lose! Scissors lose to paper")
-                computerScore++;
+                score = false;
                 break;
             case "scissors":
                 console.log("It's a tie! You both choose scissors")
@@ -71,15 +77,72 @@ function playRound(randomChoice, humanChoice){
     }else{
         console.log("Error, something went wrong")
     }
+    
+    console.log("Computer choice: " + randomChoice)
+    console.log("Player choice: " + humanChoice)
+
+    return score
 }
 
-let randomChoice = getComputerChoice().toLowerCase();
-let humanChoice = getHumanChoice().toLowerCase();
-console.log("Computer choice: " + randomChoice)
-console.log("Player choice: " + humanChoice)
-let computerScore = 0;
-let humanScore = 0;
-console.log("Computer score: " + computerScore)
-console.log("Player score: " + humanScore)
-playRound(randomChoice,humanChoice);
+//plays the whole game which is 5 rounds
+function playGame(){
+
+    let computerScore = 0;
+    let humanScore = 0;
+    let ties = 0;
+
+    let round1 = playRound()
+    console.log(round1)
+    if(round1 === true){
+        humanScore++
+    }else if (round1 === false){
+        computerScore++
+    }else{
+        ties++
+    }
+    let round2 = playRound()
+    console.log(round2)
+    if(round2 === true){
+        humanScore++
+    }else if (round2 === false){
+        computerScore++
+    }else{
+        ties++
+    }
+    let round3 = playRound()
+    console.log(round3)
+    if(round3 === true){
+        humanScore++
+    }else if (round3 === false){
+        computerScore++
+    }else{
+        ties++
+    }
+    let round4 = playRound()
+    console.log(round4)
+    if(round4 === true){
+        humanScore++
+    }else if (round4 === false){
+        computerScore++
+    }else{
+        ties++
+    }
+    let round5 = playRound()
+    console.log(round5)
+    if(round5 === true){
+        humanScore++
+    }else if (round5 === false){
+        computerScore++
+    }else{
+        ties++
+    }
+    console.log("Score is: ")
+    console.log("Computer: " + computerScore)
+    console.log("Player: " + humanScore)
+    console.log("Ties: " + ties)
+
+}
+
+playGame();
+
 
